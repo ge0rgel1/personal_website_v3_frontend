2ec2b554f080b6601e91f0244bc6bf6958b5c06a
ge0rgel1 <greatge0rgeli99@gmail.com>
2025-08-23 21:36:30 -0400

    modify db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -7,7 +7,7 @@ const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: process.env.DB_HOST?.includes('neon.tech') ? { rejectUnauthorized: false } : false, // SSL for Neon
+  ssl: true,
   max: 20, // Maximum number of connections in the pool
   idleTimeoutMillis: 30000, // Close idle connections after 30 seconds
   connectionTimeoutMillis: 2000, // Return error after 2 seconds if connection could not be established
